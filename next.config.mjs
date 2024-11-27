179d033acbb1bd08485341c4a35ad02c2b60db4c
Brent Gorwin <brent.gorwin@gmail.com>
2024-11-26 19:24:47 -0600

    Fixed github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export",
-	basePath: "/portfolio-2024", // Replace with your GitHub repo name
-	assetPrefix: "/portfolio-2024/", // Replace with your GitHub repo name
 };
 
 export default nextConfig;
